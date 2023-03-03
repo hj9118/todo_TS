54edbe5c68e22983fa7caced93e9f0df555f1d06
jiho9118 <mexocoxon@gmail.com>
2023-03-04 00:03:49 +0900

    todolist with TS

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Item } from './types/type';
+import InsertItem from './components/InsertItem';
+import ItemList from './components/ItemList';
+
+function App() {
+  const saveItem = localStorage['todos']
+    ? JSON.parse(localStorage.getItem('todos') || '')
+    : [];
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(items));
+  });
+  return (
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div className='header'>
+          <h1>{`Todo List`}</h1>
+        </div>
+        <InsertItem items={items} setItems={setItems} />
+        <ItemList items={items} setItems={setItems} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InsertItem.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Item } from '../types/type';
+import { v4 as uuid } from 'uuid';
+
+interface Props {
+  items: Item[];
+  setItems: React.Dispatch<React.SetStateAction<Item[]>>;
+}
+
+const InsertItem = ({ items, setItems }: Props) => {
+  const [itemName, setItemName] = useState('');
+  const changeItemName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(e.target.value);
+  };
+
+  const clickButton = () => {
+    setItems([...items, { itemId: uuid(), itemName, clear: false }]);
+  };
+
+  return (
+    <>
+      <input className="task-input" type='text' value={itemName} onChange={changeItemName} />{' '}
+      <button className='button-add' onClick={clickButton}>{'생성'}</button>
+    </>
+  );
+};
+
+export default InsertItem;
